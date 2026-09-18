4e877b98cfcb44d316b08db1fa355f7429df02d8
Dija89 <diallokhady1606@gmail.com>
2026-09-18 09:56:28 +0000

    Refactor StageService to use exceptions and simplify code

--- src/main/java/com/groupeisi/HelloSpring/services/StageService.java
+++ src/main/java/com/groupeisi/HelloSpring/services/StageService.java
@@ -1,12 +1,12 @@
 package com.groupeisi.HelloSpring.services;
 
 import com.groupeisi.HelloSpring.entities.Stage;
+import com.groupeisi.HelloSpring.exceptions.StageNotFoundException;
 import com.groupeisi.HelloSpring.repositories.StageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,55 +22,64 @@ public class StageService {
 
 
     // Rechercher un stage par son id
-    public Optional<Stage> findById(Long id) {
-        return stageRepository.findById(id);
+    public Stage findById(Long id) {
+
+        return stageRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new StageNotFoundException(id)
+                );
     }
 
 
-    // Créer un nouveau stage
+    // Créer un stage
     public Stage create(Stage stage) {
         return stageRepository.save(stage);
     }
 
 
-    // Modifier un stage existant
+    // Modifier un stage
     public Stage update(Long id, Stage stage) {
 
-        Optional<Stage> stageBd =
-                stageRepository.findById(id);
+        Stage stageExistant =
+                stageRepository
+                        .findById(id)
+                        .orElseThrow(
+                                () -> new StageNotFoundException(id)
+                        );
 
-        if (stageBd.isPresent()) {
 
-            Stage stageExistant =
-                    stageBd.get();
+        stageExistant.setSujetDefinitif(
+                stage.getSujetDefinitif()
+        );
 
-            stageExistant.setSujetDefinitif(
-                    stage.getSujetDefinitif()
-            );
+        stageExistant.setDateDebut(
+                stage.getDateDebut()
+        );
 
-            stageExistant.setDateDebut(
-                    stage.getDateDebut()
-            );
+        stageExistant.setDateFin(
+                stage.getDateFin()
+        );
 
-            stageExistant.setDateFin(
-                    stage.getDateFin()
-            );
+        stageExistant.setStatut(
+                stage.getStatut()
+        );
 
-            stageExistant.setStatut(
-                    stage.getStatut()
-            );
 
-            return stageRepository.save(
-                    stageExistant
-            );
-        }
-
-        return null;
+        return stageRepository.save(stageExistant);
     }
 
 
     // Supprimer un stage
     public void delete(Long id) {
-        stageRepository.deleteById(id);
+
+        Stage stage =
+                stageRepository
+                        .findById(id)
+                        .orElseThrow(
+                                () -> new StageNotFoundException(id)
+                        );
+
+        stageRepository.delete(stage);
     }
 }
